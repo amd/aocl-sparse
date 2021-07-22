68832a38555c857db5127e27130560e51b7ffd07
chsankar <chithra.sankar@amd.com>
2021-07-22 08:50:05 +0530

    SPMM : Verification code modification
    Verification code modified to include a
    factor of tolerance multiplier.
    
    Signed-off-by: Chithra Sankar <chithra.sankar@amd.com>
    AMD-Internal: [CPUPL-1103]
    Change-Id: Id3ea143a9d00f13330680afba6e7829b11c32c1e

--- tests/include/aoclsparse_check.hpp
+++ tests/include/aoclsparse_check.hpp
@@ -1,5 +1,5 @@
 /* ************************************************************************
- * Copyright (c) 2020 Advanced Micro Devices, Inc.
+ * Copyright (c) 2020-21 Advanced Micro Devices, Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -27,52 +27,87 @@
 
 #include <aoclsparse.h>
 
+#define MAX_TOL_MULTIPLIER 4
 
 template <typename T>
 void near_check_general(aoclsparse_int M, aoclsparse_int N, aoclsparse_int lda, T* refOut, T* actOut);
 
 template <>
 inline void near_check_general(
-    aoclsparse_int M, aoclsparse_int N, aoclsparse_int lda, float* refOut, float* actOut)
+	aoclsparse_int M, aoclsparse_int N, aoclsparse_int lda, float* refOut, float* actOut)
 {
+    int tolm = 1;
     for(aoclsparse_int j = 0; j < N; ++j)
     {
-        for(aoclsparse_int i = 0; i < M; ++i)
-        {
-            float compare_val = std::max(std::abs(refOut[i + j * lda] * 1e-3f),
-                                         10 * std::numeric_limits<float>::epsilon());
-            if(std::abs(refOut[i + j * lda] - actOut[i + j * lda]) > compare_val)
-            {
-                std::cerr.precision(12);
-                std::cerr << "ASSERT_NEAR(" << refOut[i + j * lda] << ", " << actOut[i + j * lda]
-                          << ") failed: " << std::abs(refOut[i + j * lda] - actOut[i + j * lda])
-                          << " exceeds compare_val " << compare_val << std::endl;
-                exit(EXIT_FAILURE);
-            }
-        }
+	for(aoclsparse_int i = 0; i < M; ++i)
+	{
+	    float compare_val = std::max(std::abs(refOut[i + j * lda] * 1e-3f),
+		    10 * std::numeric_limits<float>::epsilon());
+	    int k;
+	    for(k = 1; k <= MAX_TOL_MULTIPLIER; ++k)
+	    {
+		if(std::abs(refOut[i + j * lda] - actOut[i + j * lda]) <= compare_val * k)
+		{
+		    break;
+		}
+	    }
+
+	    if(k > MAX_TOL_MULTIPLIER)
+	    {
+		std::cerr.precision(12);
+		std::cerr << "ASSERT_NEAR(" << refOut[i + j * lda] << ", " << actOut[i + j * lda]
+		    << ") failed: " << std::abs(refOut[i + j * lda] - actOut[i + j * lda])
+		    << " exceeds permissive range [" << compare_val << ","
+		    << compare_val * MAX_TOL_MULTIPLIER << " ]" << std::endl;
+		exit(EXIT_FAILURE);
+	    }
+	    tolm = std::max(tolm, k);
+	}
+    }
+    if(tolm > 1)
+    {
+	std::cerr << "WARNING near_check has been permissive with a tolerance multiplier equal to "
+	    << tolm << std::endl;
     }
 }
 
 template <>
 inline void near_check_general(
-    aoclsparse_int M, aoclsparse_int N, aoclsparse_int lda, double* refOut, double* actOut)
+	aoclsparse_int M, aoclsparse_int N, aoclsparse_int lda, double* refOut, double* actOut)
 {
+    int tolm = 1;
     for(aoclsparse_int j = 0; j < N; ++j)
     {
-        for(aoclsparse_int i = 0; i < M; ++i)
-        {
-            double compare_val = std::max(std::abs(refOut[i + j * lda] * 1e-06),
-                                          10 * std::numeric_limits<double>::epsilon());
-                                          
-            if(std::abs(refOut[i + j * lda] - actOut[i + j * lda]) > compare_val)
-            {
-                std::cerr.precision(16);
-                std::cerr << "ASSERT_NEAR(" << refOut[i + j * lda] << ", " << actOut[i + j * lda]
-                          << ") failed: " << std::abs(refOut[i + j * lda] - actOut[i + j * lda])
-                          << " exceeds compare_val " << compare_val << std::endl;
-                exit(EXIT_FAILURE);
-            }
-        }
+	for(aoclsparse_int i = 0; i < M; ++i)
+	{
+	    double compare_val = std::max(std::abs(refOut[i + j * lda] * 1e-06),
+		    10 * std::numeric_limits<double>::epsilon());
+
+	    int k;
+	    for(k = 1; k <= MAX_TOL_MULTIPLIER; ++k)
+	    {
+		if(std::abs(refOut[i + j * lda] - actOut[i + j * lda]) <= compare_val * k)
+		{
+		    break;
+		}
+	    }
+
+	    if(k > MAX_TOL_MULTIPLIER)
+	    {
+		std::cerr.precision(12);
+		std::cerr << "ASSERT_NEAR(" << refOut[i + j * lda] << ", " << actOut[i + j * lda]
+		    << ") failed: " << std::abs(refOut[i + j * lda] - actOut[i + j * lda])
+		    << " exceeds permissive range [" << compare_val << ","
+		    << compare_val * MAX_TOL_MULTIPLIER << " ]" << std::endl;
+		exit(EXIT_FAILURE);
+	    }
+	    tolm = std::max(tolm, k);
+	}
+    }
+    if(tolm > 1)
+    {
+	std::cerr << "WARNING near_check has been permissive with a tolerance multiplier equal to "
+	    << tolm << std::endl;
     }
 }
 #endif // AOCLSPARSE_CHECK_HPP
