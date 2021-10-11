e6717305fd5f22d07e679b4425345060b6492982
chsankar <chithra.sankar@amd.com>
2021-10-11 15:19:35 +0530

    Sparse: Api Guide renamed
    Sample application moved to examples folder.
    README for running sample application added.
    
    Signed-off-by: Chithra Sankar <chithra.sankar@amd.com>
    Change-Id: I6eec6b9f579c1293315cf3ded1a4ba2eef41b5fc

--- tests/examples/sample_csrmv.cpp
+++ tests/examples/sample_csrmv.cpp
@@ -1,16 +1,16 @@
 /* ************************************************************************
- * Copyright (c) 2020 Advanced Micro Devices, Inc.
- * 
+ * Copyright (c) 2020-2021 Advanced Micro Devices, Inc.
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -18,17 +18,18 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
- * 
+ *
  * ************************************************************************ */
 
 #include "aoclsparse.h"
 #include <iostream>
 
+#define M 5
+#define N 5
+#define NNZ 8
+
 int main(int argc, char* argv[])
 {
-    aoclsparse_int         M         = 5;
-    aoclsparse_int         N         = 5;
-    aoclsparse_int         nnz       = 8;
     aoclsparse_operation   trans     = aoclsparse_operation_none;
 
     double alpha = 1.0;
@@ -48,19 +49,19 @@ int main(int argc, char* argv[])
 
     // Initialise matrix
     aoclsparse_int csr_row_ptr[M+1] = {0, 2, 3, 4, 7, 8};
-    aoclsparse_int csr_col_ind[nnz]= {0, 3, 1, 2, 1, 3, 4, 4};
-    double         csr_val[nnz] = {1 , 6 , 1.050e+01, 1.500e-02, 2.505e+02, -2.800e+02 , 3.332e+01 , 1.200e+01};
+    aoclsparse_int csr_col_ind[NNZ]= {0, 3, 1, 2, 1, 3, 4, 4};
+    double         csr_val[NNZ] = {1 , 6 , 1.050e+01, 1.500e-02, 2.505e+02, -2.800e+02 , 3.332e+01 , 1.200e+01};
     // Initialise vectors
     double x[N] = { 1.0, 2.0, 3.0, 4.0, 5.0};
     double y[M];
-    
+
     std::cout << "Invoking aoclsparse_dcsrmv..";
     //Invoke SPMV API for CSR storage format(double precision)
     aoclsparse_dcsrmv(trans,
                       &alpha,
                       M,
                       N,
-                      nnz,
+                      NNZ,
                       csr_val,
                       csr_col_ind,
                       csr_row_ptr,
--- tests/test.sh
+++
@@ -1,5 +0,0 @@
-#Set AOCLSPARSE_ROOT to library installed path
-#export AOCLSPARSE_ROOT=<path-to-library-installaton>
-#Add aoclsparse shared library path to env path variable $LD_LIBRARY_PATH
-#export LD_LIBRARY_PATH=<path-to-library-installaton>/lib:$LD_LIBRARY_PATH
-g++ -O3 -DNDEBUG sample_csrmv.cpp -I $AOCLSPARSE_ROOT/include/ -L$AOCLSPARSE_ROOT/lib/ -laoclsparse -o test
