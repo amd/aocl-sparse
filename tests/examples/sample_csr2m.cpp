84e937641ae491e677b24b8ff7412aefe499e6fa
Jan Fiala <Jan.Fiala@amd.com>
2023-03-17 16:25:47 +0000

    Restructure example for CSR2M to avoid memory leak
    aoclsparse_destroy() doesn't free arrays which where used
    to construct aoclsparse_matrix by aoclsparse_create_*csr().
    Therefore we cannot allocate these arrays in extra function
    outside of main. Rather do all in main() with existing
    local arrays and no deallocation is needed.
    
    Signed-off-by: janfiala
    AMD-Internal: CPUPL-2720
    Change-Id: I4c21acfb25dc64f654175bf5513d9fd495ec685f

--- tests/examples/sample_csr2m.cpp
+++ tests/examples/sample_csr2m.cpp
@@ -1,5 +1,5 @@
 /* ************************************************************************
- * Copyright (c) 2021-2022 Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (c) 2021-2023 Advanced Micro Devices, Inc. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -27,76 +27,19 @@
 #include <cstring>
 #include <iostream>
 
-/*Computes multiplication of 2 sparse matrices A and B
- * A is a M x K sparse matrix with nnz_A number of non-zeroes
- * B is a K x N sparse matrix with nnz_B number of non-zeroes
- * */
-#define M 3
-#define N 3
-#define K 3
-#define NNZ_A 6
-#define NNZ_B 4
+/* Computes multiplication of 2 sparse matrices A and B in CSR format. */
 
-//Comment this out for single stage computation
+// Comment this out for single stage computation
 #define TWO_STAGE_COMPUTATION
 
 #define PRINT_OUTPUT
-
-void aoclsparse_create_csr(aoclsparse_matrix    &csrA,
-                           aoclsparse_matrix    &csrB,
-                           aoclsparse_mat_descr *descrA,
-                           aoclsparse_mat_descr *descrB)
-{
-    // Create matrix descriptor of input A
-    // aoclsparse_create_mat_descr set aoclsparse_matrix_type to aoclsparse_matrix_type_general
-    // and aoclsparse_index_base to aoclsparse_index_base_zero.
-    aoclsparse_create_mat_descr(descrA);
-
-    // Create matrix descriptor of input B
-    // aoclsparse_create_mat_descr set aoclsparse_matrix_type to aoclsparse_matrix_type_general
-    // and aoclsparse_index_base to aoclsparse_index_base_zero.
-    aoclsparse_create_mat_descr(descrB);
-
-    aoclsparse_index_base base = aoclsparse_index_base_zero;
-    // Initialise matrix A
-    // 	1  0  2
-    // 	0  0  3
-    // 	4  5  6
-    aoclsparse_int row_ptr_A[] = {0, 2, 3, 6};
-    aoclsparse_int col_ind_A[] = {0, 2, 2, 0, 1, 2};
-    float          val_A[]     = {1, 2, 3, 4, 5, 6};
-    ;
-    aoclsparse_int *csr_row_ptr_A = (aoclsparse_int *)malloc((M + 1) * sizeof(aoclsparse_int));
-    aoclsparse_int *csr_col_ind_A = (aoclsparse_int *)malloc(NNZ_A * sizeof(aoclsparse_int));
-    float          *csr_val_A     = (float *)malloc((NNZ_A) * sizeof(float));
-    memcpy(csr_row_ptr_A, row_ptr_A, (M + 1) * sizeof(aoclsparse_int));
-    memcpy(csr_col_ind_A, col_ind_A, NNZ_A * sizeof(aoclsparse_int));
-    memcpy(csr_val_A, val_A, NNZ_A * sizeof(float));
-    aoclsparse_create_scsr(csrA, base, M, K, NNZ_A, csr_row_ptr_A, csr_col_ind_A, csr_val_A);
-
-    // Initialise matrix B
-    // 	1  2  0
-    // 	0  0  3
-    // 	0  4  0
-    aoclsparse_int  row_ptr_B[K + 1] = {0, 2, 3, 4};
-    aoclsparse_int  col_ind_B[NNZ_B] = {0, 1, 2, 1};
-    float           val_B[NNZ_B]     = {1, 2, 3, 4};
-    aoclsparse_int *csr_row_ptr_B    = (aoclsparse_int *)malloc((K + 1) * sizeof(aoclsparse_int));
-    aoclsparse_int *csr_col_ind_B    = (aoclsparse_int *)malloc(NNZ_B * sizeof(aoclsparse_int));
-    float          *csr_val_B        = (float *)malloc((NNZ_B) * sizeof(float));
-    memcpy(csr_row_ptr_B, row_ptr_B, (K + 1) * sizeof(aoclsparse_int));
-    memcpy(csr_col_ind_B, col_ind_B, NNZ_B * sizeof(aoclsparse_int));
-    memcpy(csr_val_B, val_B, NNZ_B * sizeof(float));
-    aoclsparse_create_scsr(csrB, base, K, N, NNZ_B, csr_row_ptr_B, csr_col_ind_B, csr_val_B);
-    return;
-}
 
 int main(int argc, char *argv[])
 {
     aoclsparse_status     status;
     aoclsparse_int        nnz_C;
     aoclsparse_request    request;
-    aoclsparse_index_base base;
+    aoclsparse_index_base base   = aoclsparse_index_base_zero;
     aoclsparse_operation  transA = aoclsparse_operation_none;
     aoclsparse_operation  transB = aoclsparse_operation_none;
 
@@ -108,7 +51,33 @@ int main(int argc, char *argv[])
     aoclsparse_mat_descr descrB;
     aoclsparse_matrix    csrA;
     aoclsparse_matrix    csrB;
-    aoclsparse_create_csr(csrA, csrB, &descrA, &descrB);
+
+    // Create matrix descriptor of input matrices
+    // aoclsparse_create_mat_descr set aoclsparse_matrix_type to aoclsparse_matrix_type_general
+    // and aoclsparse_index_base to aoclsparse_index_base_zero.
+    aoclsparse_create_mat_descr(&descrA);
+    aoclsparse_create_mat_descr(&descrB);
+
+    // Matrix sizes
+    aoclsparse_int m = 3, n = 3, k = 3;
+    aoclsparse_int nnz_A = 6, nnz_B = 4;
+    // Matrix A
+    // 	1  0  2
+    // 	0  0  3
+    // 	4  5  6
+    aoclsparse_int row_ptr_A[] = {0, 2, 3, 6};
+    aoclsparse_int col_ind_A[] = {0, 2, 2, 0, 1, 2};
+    float          val_A[]     = {1, 2, 3, 4, 5, 6};
+    aoclsparse_create_scsr(csrA, base, m, k, nnz_A, row_ptr_A, col_ind_A, val_A);
+
+    // Matrix B
+    // 	1  2  0
+    // 	0  0  3
+    // 	0  4  0
+    aoclsparse_int row_ptr_B[] = {0, 2, 3, 4};
+    aoclsparse_int col_ind_B[] = {0, 1, 2, 1};
+    float          val_B[]     = {1, 2, 3, 4};
+    aoclsparse_create_scsr(csrB, base, k, n, nnz_B, row_ptr_B, col_ind_B, val_B);
 
     aoclsparse_matrix csrC          = NULL;
     aoclsparse_int   *csr_row_ptr_C = NULL;
