c8b7ddf9eb1247ebacaf747e767a542b136707d0
Andrew Sajo <andrew.sajo@amd.com>
2023-06-14 18:56:35 +0100

    Add Copyright banner to tool scripts
    Add missing copyright info to some
    scripts and CMake lists.
    
    Copy right years are taken from git log <file>
    
    Signed-off-by: Andrew Sajo <andrsajo@amd.com>
    AMD-Internal: CPUPL-3567
    
    Change-Id: I51af066391b631b9830c7c6e485f20c90278f85b

--- tools/check_copyright.sh
+++ tools/check_copyright.sh
@@ -23,34 +23,58 @@
 # Check if all files have adjusted current copyright year
 # and return number of non-matching files.
 # It is assumed that the copyright label should on the top of the files
-# in the form: ' * Copyright (c) 2020-2023' or ' * Copyright (c) 2023'
+# in the form: 'Copyright (c) 2020-2023' or 'Copyright (c) 2023'
 
 print_help() {
-  echo $0 [file1 [file2 ...]]
+  echo $0 [-p] [file1 [file2 ...]]
   echo Checks if files have adjusted copyright to the current year
   echo and return the number of non-matching files.
   echo If no file is given, all staged files are checked.
+  echo
+  echo '-p flag indicates to print the copyright header for ANY year or'
+  echo a message indicating no copyright info found. The flag NEEDS to
+  echo be the first argument.
 }
+
+PRINT=
+
+if [ "$#" -gt 0 ]; then
+  if [ "$1" = "-p" ]; then
+    PRINT='-p'
+    shift
+  fi
+fi
 
 if [ "$#" -eq 0 ]; then
   print_help
   echo ""
   echo "Testing all staged files"
-  git status --porcelain | grep '^[AM]' | cut -c4- | xargs --no-run-if-empty bash $0
+  git status --porcelain | grep '^[AM]' | cut -c4- | xargs --no-run-if-empty bash $0 $PRINT
   exit
 fi
 
-current_year=$(date '+%Y')
 nfailed=0
+
+if [ ! -z "$PRINT" ]; then
+  current_year='\([0-9]\{4\}\)'
+  message=" does NOT include copyright banner"
+  function prn () { echo "$1:" \"$2\" ; }
+else
+  current_year=$(date '+%Y')
+  message=" does NOT include $current_year in the copyright"
+  function prn () { echo "$1" is ok. ; }
+fi
 
 for f in "$@"; do
 
-  head -3 "$f" | grep --silent "Copyright (c) \([0-9]\{4\}-\)\?$current_year"
+  CP=`head -3 "$f" | grep "Copyright (c) \([0-9]\{4\}-\)\?$current_year"`
   if [ "$?" -ne 0 ]; then
-    echo \>\>\>"$f" does NOT include $current_year in the copyright
+    # Notify problem
+    echo \>\>\>"$f" $message
     let nfailed++
   else
-    echo "$f" is ok
+    # print OK
+    prn "$f" "$CP"
   fi
 
 done
