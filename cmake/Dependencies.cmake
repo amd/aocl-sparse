3815911a14ea9ce324ce2e49f6cef15c943caba8
Vishwanath Chunchula <Vishwanath.Chunchula@amd.com>
2023-08-17 03:50:24 -0400

    Support to link to AOCL Utils library
    AMD-Internal: CPUPL-3784
    Change-Id: Ia88630e37ac2e350afe7b00d550dba60153a1a22

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1,5 +1,5 @@
-# ########################################################################
-# Copyright (c) 2020 Advanced Micro Devices, Inc.
+# ##############################################################################
+# Copyright (c) 2020-2023 Advanced Micro Devices, Inc.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -8,29 +8,187 @@
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
 #
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
 #
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 #
-# ########################################################################
+# ##############################################################################
 
-# Dependencies
+# ============= aocl function ================
+function(aocl_libs)
 
-# Git
+  if(BUILD_ILP64)
+    set(ILP_DIR "ILP64")
+  else(BUILD_ILP64)
+    set(ILP_DIR "LP64")
+  endif(BUILD_ILP64)
+
+  if(WIN32)
+    set(CMAKE_FIND_LIBRARY_PREFIXES "")
+    set(CMAKE_FIND_LIBRARY_SUFFIXES ".lib")
+    set(_blas_library "AOCL-LibBlis-Win")
+    set(_flame_library "AOCL-LibFlame-Win")
+    set(_utils_library "libaoclutils")
+    set(_static_tag "static")
+    set(_mt_tag "MT")
+
+    if(SUPPORT_OMP)
+      set(_blas_static_library "${_blas_library}-${_mt_tag}")
+      set(_blas_dyn_library "${_blas_static_library}-dll")
+      set(_flame_static_library "${_flame_library}-${_mt_tag}")
+      set(_flame_dyn_library "${_flame_static_library}-dll")
+    else(SUPPORT_OMP)
+      set(_blas_static_library "${_blas_library}")
+      set(_blas_dyn_library "${_blas_static_library}-dll")
+      set(_flame_static_library "${_flame_library}")
+      set(_flame_dyn_library "${_flame_static_library}-dll")
+    endif(SUPPORT_OMP)
+
+    set(_utils_static_library "${_utils_library}_${_static_tag}")
+    set(_utils_dyn_library "${_utils_library}")
+
+  else(WIN32)
+    set(CMAKE_FIND_LIBRARY_PREFIXES "lib")
+    if(BUILD_SHARED_LIBS)
+      set(CMAKE_FIND_LIBRARY_SUFFIXES ".so")
+    else(BUILD_SHARED_LIBS)
+      set(CMAKE_FIND_LIBRARY_SUFFIXES ".a")
+    endif(BUILD_SHARED_LIBS)
+    set(_blas_library "blis")
+    set(_flame_library "flame")
+    set(_mt_tag "mt")
+    set(_utils_static_library "aoclutils")
+    set(_utils_dyn_library "aoclutils")
+
+    if(SUPPORT_OMP)
+      set(_blas_static_library "${_blas_library}-${_mt_tag}")
+      set(_blas_dyn_library "${_blas_library}-${_mt_tag}")
+      set(_flame_static_library "${_flame_library}")
+      set(_flame_dyn_library "${_flame_library}")
+    else(SUPPORT_OMP)
+      set(_blas_static_library "${_blas_library}")
+      set(_blas_dyn_library "${_blas_library}")
+      set(_flame_static_library "${_flame_library}")
+      set(_flame_dyn_library "${_flame_library}")
+    endif(SUPPORT_OMP)
+  endif(WIN32)
+
+  # Link against dynamic library by default
+  find_library(
+    AOCL_UTILS_LIB
+    NAMES ${_utils_dyn_library} ${_utils_static_library} NAMES_PER_DIR
+    HINTS ${AOCL_ROOT}/utils ${AOCL_ROOT}/amd-utils ${AOCL_ROOT}
+    PATH_SUFFIXES "lib/${ILP_DIR}" "lib_${ILP_DIR}" "lib"
+    DOC "AOCL Utils library")
+
+  find_library(
+    AOCL_BLIS_LIB
+    NAMES ${_blas_static_library} ${_blas_dyn_library}
+    HINTS ${AOCL_ROOT}/blis ${AOCL_ROOT}/amd-blis ${AOCL_ROOT}
+    PATH_SUFFIXES "lib/${ILP_DIR}" "lib_${ILP_DIR}" "lib"
+    DOC "AOCL Blis library")
+
+  find_library(
+    AOCL_LIBFLAME
+    NAMES ${_flame_dyn_library} ${_flame_static_library} NAMES_PER_DIR
+    HINTS ${AOCL_ROOT}/libflame ${AOCL_ROOT}/amd-libflame ${AOCL_ROOT}
+    PATH_SUFFIXES "lib/${ILP_DIR}" "lib_${ILP_DIR}" "lib"
+    DOC "AOCL LIBFLAME library")
+  # ====Headers
+  find_path(
+    AOCL_UTILS_INCLUDE_DIR
+    NAMES alci_c.h alci.h arch.h cache.h enum.h macros.h
+    HINTS ${AOCL_ROOT}/amd-utils ${AOCL_ROOT}/utils ${AOCL_ROOT}
+    PATH_SUFFIXES "include/${ILP_DIR}/alci" "include_${ILP_DIR}/alci"
+                  "include/alci"
+    DOC "AOCL Utils headers")
+
+  find_path(
+    AOCL_BLIS_INCLUDE_DIR
+    NAMES blis.h cblas.h blis.hh cblas.hh
+    HINTS ${AOCL_ROOT}/amd-blis ${AOCL_ROOT}/blis ${AOCL_ROOT}
+    PATH_SUFFIXES "include/${ILP_DIR}" "include_${ILP_DIR}" "include"
+                  "include/blis"
+    DOC "AOCL Blis headers")
+  find_path(
+    AOCL_LIBFLAME_INCLUDE_DIR
+    NAMES FLAME.h lapacke.h lapacke_mangling.h lapack.h libflame.hh
+          libflame_interface.hh
+    HINTS ${AOCL_ROOT}/libflame ${AOCL_ROOT}/amd-libflame ${AOCL_ROOT}
+    PATH_SUFFIXES "include/${ILP_DIR}" "include_${ILP_DIR}" "include"
+    DOC "AOCL Libflame headers")
+
+  # ===========
+  if(AOCL_BLIS_LIB
+     AND AOCL_LIBFLAME
+     AND AOCL_BLIS_INCLUDE_DIR
+     AND AOCL_LIBFLAME_INCLUDE_DIR
+     AND AOCL_UTILS_LIB
+     AND AOCL_UTILS_INCLUDE_DIR)
+    set(LAPACK_AOCL_FOUND
+        true
+        PARENT_SCOPE)
+    set(LAPACK_LIBRARY ${AOCL_LIBFLAME} ${AOCL_BLIS_LIB} ${AOCL_UTILS_LIB})
+  else()
+    message(
+      FATAL_ERROR
+        "Error: could not find a suitable installation of Blas/Lapack/Utils Libraries in \$AOCL_ROOT=${AOCL_ROOT}"
+    )
+  endif()
+
+  set(LAPACK_LIBRARY
+      ${LAPACK_LIBRARY}
+      PARENT_SCOPE)
+
+endfunction(aocl_libs)
+
+# ==================main=================
+
+# Add external dependencies such as Git, OpenMP Git
 find_package(Git REQUIRED)
-
 # Find OpenMP package
 find_package(OpenMP)
-if (NOT OPENMP_FOUND)
+if(NOT OPENMP_FOUND)
   message("-- OpenMP not found.")
 else()
   option(SUPPORT_OMP "Compile WITH OpenMP support." ON)
 endif()
 
+# clear to avoid endless appending on subsequent calls
+set(LAPACK_LIBRARY)
+set(BLAS_LIBRARY)
+unset(LAPACK_INCLUDE_DIR)
+
+if(DEFINED ENV{AOCL_ROOT})
+  set(AOCL_ROOT $ENV{AOCL_ROOT})
+else()
+  set(AOCL_ROOT ${CMAKE_INSTALL_PREFIX})
+endif()
+
+# find AOCL dependencies such as Blis, Libflame
+aocl_libs()
+
+set(LAPACK_LIBRARIES ${LAPACK_LIBRARY})
+set(BLIS_INCLUDE_DIRS ${AOCL_BLIS_INCLUDE_DIR})
+set(LAPACK_INCLUDE_DIRS ${AOCL_LIBFLAME_INCLUDE_DIR})
+# Utils package is built with relative header path inclusion. Finding absolute
+# path results in compilation errors in windows builds. So path is adjusted to a
+# folder under "include" that contains "alci" folder. Therefore, the below path
+# to Utils Headers is adjusted. If we can get the right path using find_path( ),
+# then we can remove this adjustment.
+set(UTILS_INCLUDE_DIRS ${AOCL_UTILS_INCLUDE_DIR}/..)
+
+message(STATUS "Dependencies (libraries and includes)")
+message(STATUS "  \$LAPACK LIBRARIES......${LAPACK_LIBRARIES}")
+message(STATUS "  \$LAPACK_INCLUDE_DIRS...${LAPACK_INCLUDE_DIRS}")
+message(STATUS "  \$BLIS_INCLUDE_DIRS.....${BLIS_INCLUDE_DIRS}")
+message(STATUS "  \$UTILS_INCLUDE_DIRS....${UTILS_INCLUDE_DIRS}")
+
+mark_as_advanced(LAPACK_LIBRARIES LAPACK_INCLUDE_DIRS BLIS_INCLUDE_DIRS)
