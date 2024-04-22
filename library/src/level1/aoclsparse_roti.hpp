b0af0fcde8056e718905a7f70f053c6eee573617
Harihara Sudhan S <HariharaSudhan.S@amd.com>
2024-04-21 23:52:25 -0700

    KT kernel for roti
    - Added KT-based kernel for roti.
    - Guarded the KT-based kernel' instantiation using
      appropriate flag macro.
    
    Signed-off-by: Harihara Sudhan S, Andrew Sajo
    AMD-Internal: [CPUPL-3734]
    Change-Id: Ib1d15eb664f13900d7581e33790197b0f97efccb

--- library/src/level1/aoclsparse_roti.hpp
+++ library/src/level1/aoclsparse_roti.hpp
@@ -1,5 +1,5 @@
 /* ************************************************************************
- * Copyright (c) 2023 Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (c) 2023-2024 Advanced Micro Devices, Inc. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -23,17 +23,11 @@
  */
 #ifndef AOCLSPARSE_ROTI_HPP
 #define AOCLSPARSE_ROTI_HPP
-#endif
 
 #include "aoclsparse.h"
 #include "aoclsparse_context.h"
-
-#include <complex>
-
-#if defined(_WIN32) || defined(_WIN64)
-//Windows equivalent of gcc c99 type qualifier __restrict__
-#define __restrict__ __restrict
-#endif
+#include "aoclsparse_kernel_templates.hpp"
+#include "aoclsparse_utils.hpp"
 
 /*
  * Reference implementation for Givens rotation
@@ -45,9 +39,8 @@ inline aoclsparse_status roti_ref(aoclsparse_int nnz,
                                   T *__restrict__ x,
                                   const aoclsparse_int *__restrict__ indx,
                                   T *__restrict__ y,
-                                  T                                     c,
-                                  T                                     s,
-                                  [[maybe_unused]] const aoclsparse_int kid)
+                                  T c,
+                                  T s)
 {
     aoclsparse_int i;
     T              temp;
@@ -63,6 +56,69 @@ inline aoclsparse_status roti_ref(aoclsparse_int nnz,
     return aoclsparse_status_success;
 }
 
+using namespace kernel_templates;
+
+/*
+ * KT implementation for Givens rotation
+ * It is assumed that all pointers and data are valid.
+ * The indx vector is NOT checked for non-negative values.
+ */
+template <bsz SZ, typename SUF>
+inline aoclsparse_status roti_kt(aoclsparse_int nnz,
+                                 SUF *__restrict__ x,
+                                 const aoclsparse_int *__restrict__ indx,
+                                 SUF *__restrict__ y,
+                                 SUF c,
+                                 SUF s)
+{
+    // Automatically determine the type of tsz
+    constexpr aoclsparse_int tsz = tsz_v<SZ, SUF>;
+
+    aoclsparse_int vc  = nnz / tsz;
+    aoclsparse_int rem = nnz % tsz;
+
+    avxvector_t<SZ, SUF> yv, xv, cv, sv;
+    avxvector_t<SZ, SUF> tmp[2];
+
+    // Broadcast the constants
+    cv = kt_set1_p<SZ, SUF>(c);
+    sv = kt_set1_p<SZ, SUF>(s);
+
+    for(aoclsparse_int i = 0; i < vc; i++)
+    {
+        // Load the vectors
+        xv = kt_loadu_p<SZ, SUF>(x + (i * tsz));
+        yv = kt_set_p<SZ, SUF>(y, indx + (i * tsz));
+
+        // tmp = x * c
+        tmp[0] = kt_mul_p<SZ, SUF>(cv, xv);
+
+        // tmp = x * s
+        tmp[1] = kt_mul_p<SZ, SUF>(sv, xv);
+
+        // tmp = y * s + x_tmp
+        tmp[0] = kt_fmadd_p<SZ, SUF>(sv, yv, tmp[0]);
+
+        // y_tmp = x * c - x_tmp
+        tmp[1] = kt_fmsub_p<SZ, SUF>(cv, yv, tmp[1]);
+
+        // Store the results
+        kt_storeu_p<SZ, SUF>(x + (i * tsz), tmp[0]);
+        kt_scatter_p<SZ, SUF>(tmp[1], y, indx + (i * tsz));
+    }
+
+    SUF temp;
+
+    for(aoclsparse_int i = nnz - rem; i < nnz; i++)
+    {
+        temp       = x[i];
+        x[i]       = c * x[i] + s * y[indx[i]];
+        y[indx[i]] = c * y[indx[i]] - s * temp;
+    }
+
+    return aoclsparse_status_success;
+}
+
 /*
  * aoclsparse_roti dispatcher
  */
@@ -71,9 +127,9 @@ inline aoclsparse_status aoclsparse_rot(aoclsparse_int nnz,
                                         T *__restrict__ x,
                                         const aoclsparse_int *__restrict__ indx,
                                         T *__restrict__ y,
-                                        T                                     c,
-                                        T                                     s,
-                                        [[maybe_unused]] const aoclsparse_int kid)
+                                        T              c,
+                                        T              s,
+                                        aoclsparse_int kid = -1)
 {
     // Check pointer arguments
     if((nullptr == x) || (nullptr == indx) || (nullptr == y))
@@ -91,8 +147,30 @@ inline aoclsparse_status aoclsparse_rot(aoclsparse_int nnz,
     {
         return aoclsparse_status_invalid_size;
     }
-    else
+
+    // Creating pointer to the kernel
+    using K = decltype(&roti_ref<T>);
+
+    K kernel;
+
+    // TODO: Replace with L1 dispatcher
+    // Pick the kernel based on the KID passed
+    switch(kid)
     {
-        return roti_ref<T>(nnz, x, indx, y, c, s, kid);
+    case 2:
+#if defined(__AVX512F__)
+        kernel = roti_kt<bsz::b512, T>;
+        break;
+#endif
+    case 1:
+#if defined(__AVX2__)
+        kernel = roti_kt<bsz::b256, T>;
+        break;
+#endif
+    default:
+        kernel = roti_ref<T>;
     }
+
+    return kernel(nnz, x, indx, y, c, s);
 }
+#endif
