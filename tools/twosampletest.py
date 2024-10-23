816ed7453d053893f06d67703f75fa25a2ca76b4
Andrew Sajo <andrew.sajo@amd.com>
2024-10-23 16:11:00 +0100

    Tidying up of t-test script
    Signed-off-by: Andrew Sajo
    AMD-Internal: N/A
    Change-Id: I220042f8f706df86b5f05b40ec463b762fea73f7

--- tools/twosampletest.py
+++ tools/twosampletest.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # Copyright (c) 2024 Advanced Micro Devices, Inc. All rights reserved.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -20,19 +20,23 @@
 # THE SOFTWARE.
 ############################################################
 #
-# Perform two-sample mean t- or z-test
+# pylint: disable=too-many-locals,invalid-name,line-too-long,consider-using-f-string
+# pylint: disable=too-many-statements
+'''
+Peform two-sample mean t- or z-test
+'''
 
-import pandas as pd
-import numpy as np
-import os
 import sys
 import argparse
-from scipy import stats
-from scipy.stats import distributions
+import numpy as np
+import pandas as pd
 from scipy.stats._result_classes import TtestResult
+from scipy.stats import distributions
+from scipy import stats
 
 
 def _equal_var_ttest_denom(v1, n1, v2, n2):
+    """t-test for equal variance"""
     # If there is a single observation in one sample, this formula for pooled
     # variance breaks down because the variance of that sample is undefined.
     # The pooled variance is still defined, though, because the (n-1) in the
@@ -45,6 +49,7 @@ def _equal_var_ttest_denom(v1, n1, v2, n2):
     svar = ((n1 - 1) * v1 + (n2 - 1) * v2) / df
     denom = np.sqrt(svar * (1.0 / n1 + 1.0 / n2))
     return df, denom
+
 
 def _get_pvalue(statistic, distribution, alternative, symmetric=True):
     """Get p-value given the statistic, (continuous) distribution, and alternative"""
@@ -63,8 +68,9 @@ def _get_pvalue(statistic, distribution, alternative, symmetric=True):
 
     return pvalue
 
-def _ttest_ind_from_stats(mean1, mean2, denom, df, alternative):
 
+def _ttest_ind_from_stats(mean1, mean2, denom, df, alternative):
+    """t-test ..."""
     d = mean1 - mean2
     with np.errstate(divide='ignore', invalid='ignore'):
         t = np.divide(d, denom)[()]
@@ -72,79 +78,114 @@ def _ttest_ind_from_stats(mean1, mean2, denom, df, alternative):
 
     return (t, prob)
 
-def main(argv):
 
-    parser = argparse.ArgumentParser(description="Performa a two-sample t- or z-test")
-    _ = parser.add_argument('-f', '-i', '--filename', '--input', '-csv', default="", type=argparse.FileType('r'), required=True, help="csv filename to process")
-    _ = parser.add_argument('-c', '--confidence', help="define test significance (alpha), min: 0.1, max: 0.999", default=0.95, type=float)
-    _ = parser.add_argument('-t', '--test-type', help="choose type of test to performr", default='auto', type=str, choices=['auto', 't-student', 'normal'])
+def t_test(data, alpha=0.95, output=True):
+    '''
+    Perform t-test on the means of two groups -- on per line of data
+    '''
 
-    args = parser.parse_args(argv)
-    FILE = args.filename.name
-
-    alpha = args.confidence
-    if (alpha <0.1 or alpha > 0.999):
-        print("Warning: the provided alpha value is too extreme, setting to 0.95\n")
-        alpha = 0.95
-
-    TTEST_TYPE = args.test_type
-
-    try:
-        d = pd.read_csv(FILE, header=None, names=["pname", "meana", "sda", "na", "meanb", "sdb", "nb"])
-    except Exception:
-        print("Error: something bad occurred while opening or reading the input file: " + FILE +"...")
-        sys.exit(4)
-
-    # Perform a {t,z}-test on the means of the two groups -- on per line of d
-    for row in d.index:
-        n1 = d["na"][row]
-        n2 = d["nb"][row]
-        m1 = d["meana"][row]
-        m2 = d["meanb"][row]
-        s1 = d["sda"][row]
-        s2 = d["sdb"][row]
+    results = []
+    table = None
+    for row in data.index:
+        n1 = data["na"][row]
+        n2 = data["nb"][row]
+        m1 = data["meana"][row]
+        m2 = data["meanb"][row]
+        s1 = data["sda"][row]
+        s2 = data["sdb"][row]
         err = m1 - m2
         df = n1 + n2 - 2
         Sp = np.sqrt(((n1-1)*s1**2 + (n2-1)*s2**2)/df)
         SE = Sp * np.sqrt(1/n1 + 1/n2)
         alternative = "two-sided"
 
-        if (TTEST_TYPE == 'normal'): # or ((na >= 30 and nb >= 30) and (TTEST_TYPE == 'auto')):
-            print("Test type not yet implemented")
-            sys.exit(9)
-        else:
-            # do a t-Student test
-            # ttest = stats.ttest_ind(aok, bok, equal_var=True, alternative='two-sided')
-            v1 = s1**2
-            v2 = s2**2
-            df, denom = _equal_var_ttest_denom(v1, n1, v2, n2)
-            t, prob = _ttest_ind_from_stats(m1, m2, denom, df, alternative)
-            # when nan_policy='omit', `df` can be different for different axis-slices
-            df = np.broadcast_to(df, t.shape)[()]
-            estimate = m1-m2
-            # _axis_nan_policy decorator doesn't play well with strings
-            alternative_num = {"less": -1, "two-sided": 0, "greater": 1}[alternative]
-            ttest = TtestResult(t, prob, df=df, alternative=alternative_num,
-                               standard_error=denom, estimate=estimate)
-            t = stats.t.interval(confidence=alpha, df=df)
-            ci = err + np.array(t) * SE
-            clabel = 't-Student'
-            rlabel = 't-stats(df={})'.format(df)
+        # do a t-Student test
+        # ttest = stats.ttest_ind(aok, bok, equal_var=True, alternative='two-sided')
+        v1 = s1**2
+        v2 = s2**2
+        df, denom = _equal_var_ttest_denom(v1, n1, v2, n2)
+        t, prob = _ttest_ind_from_stats(m1, m2, denom, df, alternative)
+        # when nan_policy='omit', `df` can be different for different axis-slices
+        df = np.broadcast_to(df, t.shape)[()]
+        estimate = m1-m2
+        # _axis_nan_policy decorator doesn't play well with strings
+        alternative_num = {"less": -1,
+                           "two-sided": 0, "greater": 1}[alternative]
+        ttest = TtestResult(t, prob, df=df, alternative=alternative_num,
+                            standard_error=denom, estimate=estimate)
+        t = stats.t.interval(confidence=alpha, df=df)
+        ci = err + np.array(t) * SE
+        clabel = 't-Student'
+        rlabel = 't-stats(df={})'.format(df)
 
         # Large P-value we do not reject H0: A=B
-        if (ttest.pvalue > 1-alpha):
+        if ttest.pvalue > 1-alpha:
             result = "same"
         else:
             result = "DIFF"
+        if output:
+            stab = pd.DataFrame({'Stats': ['mean', 'stdev', 'Err(A-B)', rlabel, 'P-value', 'CI({}) L'.format(alpha), 'CI({}) U'.format(alpha)],
+                                 'Grp A(n={})'.format(n1): [m1, s1, None, None, None, None, None],
+                                 'Grp B(n={})'.format(n2): [m2, s2, None, None, None, None, None],
+                                 clabel: [None, None, err, ttest.statistic, ttest.pvalue, ci[0], ci[1]]})
+            print("* {},{}".format(data["pname"][row], result))
+            print("H0: mean(A) = mean(B)\nH1: mean(A) /= mean(B)\n")
+            # print("P-value greater than alpha indicates not enough evidence to reject H0,\nand take that the means are indistinguishable\n")
+            print(stab.to_markdown(index="never").replace("nan", "   "))
 
-        print("* {},{}".format(d["pname"][row], result))
-        print("H0: mean(A) = mean(B)\nH1: mean(A) /= mean(B)\n")
-        # print("P-value greater than alpha indicates not enough evidence to reject H0,\nand take that the means are indistinguishable\n")
-        stab = pd.DataFrame({'Stats': ['mean', 'stdev', 'Err(A-B)', rlabel, 'P-value', 'CI({}) L'.format(alpha),'CI({}) U'.format(alpha)],
-        'Grp A(n={})'.format(n1): [m1, s1,None,None,None,None,None],
-        'Grp B(n={})'.format(n2): [m2, s2,None,None,None,None,None],
-        clabel: [None, None, err,ttest.statistic,ttest.pvalue,ci[0],ci[1]]})
-        print(stab.to_markdown(index="never").replace("nan","   "))
+        mok = True
+        if m1 == 0.0:
+            print('warning: mean for group A is zero, solve bigger problem(s)')
+            mok = False
+
+        if m2 == 0.0:
+            print('warning: for group B is zero, solve bigger problem(s)')
+            mok = False
+
+        if mok:
+            speedup = m1/m2
+        else:
+            speedup = float('nan')
+            print('warning: setting speedup to NaN')
+
+        results.append(result)
+        if table is None:
+            table = pd.DataFrame({'result': result, 'p-val': ttest.pvalue, 'low': ci[0], 'upp': ci[1],
+                                  'sgnc': 1-alpha, 'dif': estimate, 'speedup': speedup}, index=[row])
+        else:
+            table = pd.concat([table, pd.DataFrame({'result': result, 'p-val': ttest.pvalue, 'low': ci[0], 'upp': ci[1],
+                               'sgnc': 1-alpha, 'dif': estimate, 'speedup': speedup}, index=[row])])
+
+    return results, table
+
+
+def main(argv):
+    '''Main entry point for two-sample t-test computatioss'''
+
+    parser = argparse.ArgumentParser(
+        description="Perform a two-sample t-test")
+    _ = parser.add_argument('-f', '-i', '--filename', '--input', '-csv',
+                            type=argparse.FileType('r'), required=True, help="csv filename to process")
+    _ = parser.add_argument(
+        '-c', '--confidence', help="define test significance (alpha), min: 0.001, max: 0.999", default=0.95, type=float)
+
+    args = parser.parse_args(argv)
+    file = args.filename.name
+
+    alpha = args.confidence
+    if alpha < 0.001 or alpha > 0.999:
+        print("Warning: the provided alpha value is too extreme, setting to 0.95\n")
+        alpha = 0.95
+
+    try:
+        d = pd.read_csv(file, header=None, names=[
+                        "pname", "meana", "sda", "na", "meanb", "sdb", "nb"])
+    except FileNotFoundError:
+        print("Error: something bad occurred while opening or reading the input file: " + file + "...")
+        sys.exit(4)
+
+    t_test(data=d, alpha=alpha)
+
 
 if __name__ == "__main__":
-   main(sys.argv[1:])
+    main(sys.argv[1:])
