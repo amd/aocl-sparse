d643a8fd4b16f12eb5808a25ad279f2f6f7c132e
Chunchula, Vishwanath <Vishwanath.Chunchula@amd.com>
2026-07-08 13:51:00 +0530

    chore: upgrade version to 5.3.1 (#148)

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ project = 'AOCL-Sparse'
 copyright = '2020-2026, Advanced Micro Devices, Inc'
 author = 'Advanced Micro Devices, Inc'
 version = ''
-release ='5.2.2'
+release ='5.3.1'
 
 # -- General configuration ---------------------------------------------------
 extensions = ['sphinxcontrib.bibtex', 'breathe', 'sphinx_collapse']
